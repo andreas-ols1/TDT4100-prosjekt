12088114e73c0a15858e807ed16f40f80388bce7
Jakob Eilertsen Punnerud <jakobep@stud.ntnu.no>
2022-03-17 16:40:37 +0100

    Edit files

--- src/main/java/PokemonShowdown/Pokemon.java
+++ src/main/java/PokemonShowdown/Pokemon.java
@@ -1,6 +1,7 @@
 package PokemonShowdown;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,36 +14,26 @@ public class Pokemon {
     private int defence;
     private int speed;
     private int hp;
+    private List<String> validPokemon = Arrays.asList("charizard", "blastoise", "venusaur", "pikachu");
 
     public Pokemon(String name) {
+        checkValidPokemon(name);
         if (name.toLowerCase().equals("charizard")) {
-            setMonToCharizard();
+            setPokemonToCharizard();
         }
         if (name.toLowerCase().equals("venusaur")) {
-            type = new Type("grass");
-            moves.add(new AttackMove("slash"));
-            moves.add(new AttackMove("earthquake"));
-            moves.add(new PassiveMove("swords dance"));
-            moves.add(new AttackMove("solar beam"));
-            attack = 100;
-            defence = 100;
-            speed = 80;
-            hp = 79;
+            setPokemonToVenusaur();
         }
         if (name.toLowerCase().equals("blastoise")) {
-            type = new Type("water");
-            moves.add(new AttackMove("hydro pump"));
-            moves.add(new PassiveMove("recover"));
-            moves.add(new AttackMove("skull bash"));
-            moves.add(new AttackMove("ice beam"));
-            attack = 85;
-            defence = 105;
-            speed = 78;
-            hp = 80;
+            setPokemonToBlastoise();
         }
     }
 
-    public void setMonToCharizard() {
+    public void checkValidPokemon(String name) {
+        if (!(validPokemon.contains(name))) throw new IllegalArgumentException("Not a valid pokémon");
+    }
+ 
+    public void setPokemonToCharizard() {
         name = "charizard";
         type = new Type("fire");
         moves.add(new AttackMove("flamethrower"));
@@ -53,6 +44,32 @@ public class Pokemon {
         defence = 80;
         speed = 100;
         hp = 78;
+    }
+
+    public void setPokemonToBlastoise() {
+        name = "blastoise";
+        type = new Type("water");
+        moves.add(new AttackMove("hydro pump"));
+        moves.add(new PassiveMove("recover"));
+        moves.add(new AttackMove("skull bash"));
+        moves.add(new AttackMove("ice beam"));
+        attack = 85;
+        defence = 105;
+        speed = 78;
+        hp = 80;
+    }
+
+    public void setPokemonToVenusaur() {
+        name = "venusaur";
+        type = new Type("grass");
+        moves.add(new AttackMove("slash"));
+        moves.add(new AttackMove("earthquake"));
+        moves.add(new PassiveMove("swords dance"));
+        moves.add(new AttackMove("solar beam"));
+        attack = 100;
+        defence = 100;
+        speed = 80;
+        hp = 79;
     }
 
     public String getName() {
