6c8267a150f832b0706463f22b479ee6a551fd23
Jakob Eilertsen Punnerud <jakobep@stud.ntnu.no>
2022-03-17 16:07:20 +0100

    Make main classes

---
+++ src/main/java/PokemonShowdown/Move.java
@@ -0,0 +1,12 @@
+package PokemonShowdown;
+
+public class Move {
+    
+    protected String name;
+    protected Type type;
+
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ src/main/java/PokemonShowdown/Type.java
@@ -0,0 +1,117 @@
+package PokemonShowdown;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Type {
+    
+    private String name;
+    private List<String> strongAgainst = new ArrayList<>();
+    private List<String> weakAgainst = new ArrayList<>();
+
+    public Type(String type) {
+        checkValidType(type);
+        if (type.equals("fire")) {
+            setFireType();
+        }
+        if (type.equals("water")) {
+            setWaterType();
+        }
+        if (type.equals("grass")) {
+            setGrassType();
+        }
+        if (type.equals("normal")) {
+            setNormalType();
+        }
+        if (type.equals("ground")) {
+            setGroundType();
+        }
+        if (type.equals("ice")) {
+            setIceType();
+        }
+    }
+
+    private void checkValidType(String type) {
+        if (!(type.equals("fire")
+        || type.equals("water")
+        || type.equals("grass")
+        || type.equals("ground")
+        || type.equals("normal")
+        || type.equals("ice"))) {
+            throw new IllegalArgumentException("Not a valid type");
+        }
+    }
+
+    private void setFireType() {
+        this.name = "fire";
+        strongAgainst.add("grass");
+        strongAgainst.add("ice");
+        weakAgainst.add("water");
+        weakAgainst.add("fire");
+    }
+
+    private void setWaterType() {
+        this.name = "water";
+        strongAgainst.add("fire");
+        strongAgainst.add("ground");
+        weakAgainst.add("grass");
+        weakAgainst.add("water");
+    }
+
+    private void setGrassType() {
+        this.name = "grass";
+        strongAgainst.add("water");
+        strongAgainst.add("ground");
+        weakAgainst.add("fire");
+        weakAgainst.add("grass");
+    }
+
+    private void setGroundType() {
+        this.name = "ground";
+        strongAgainst.add("fire");
+        weakAgainst.add("grass");
+    }
+
+    private void setNormalType() {
+        this.name = "normal";
+    }
+
+    private void setIceType() {
+        this.name = "ice";
+        strongAgainst.add("ground");
+        strongAgainst.add("grass");
+        weakAgainst.add("fire");
+        weakAgainst.add("water");
+        weakAgainst.add("ice");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean checkIfStrongAgainst(Type type1, Type type2) {
+        for (String typeName : type1.strongAgainst) {
+            if (typeName.equals(type2.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkIfWeakAgainst(Type type1, Type type2) {
+        for (String typeName : type1.weakAgainst) {
+            if (typeName.equals(type2.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + 
+        "\nStrong against: " + strongAgainst +
+        "\nWeak against: " + weakAgainst;
+    }
+    
+}
