097d708a22ab554726efc0a6304803c28375ddb4
Jakob Eilertsen Punnerud <jakobep@stud.ntnu.no>
2022-04-05 01:10:10 +0200

    Add damage and stat boosting moves

--- src/main/java/PokemonShowdown/Move.java
+++ src/main/java/PokemonShowdown/Move.java
@@ -10,90 +10,100 @@ public class Move {
     private int damage;
     private double accuracy;
     private double heal;
-    private boolean attackBoost;
+    private double attackBoost;
+    private double speedBoost;
     private Collection<String> validMoves = Arrays.asList("swords dance","recover","hydro pump","slash","flamethrower",
     "earthquake","solar beam","ice beam","skull bash","thunderbolt","surf","sludge bomb","air slash","cross chop","thunder punch",
-    "ice punch","fire punch","psychic","rock slide","shadow ball","dragon pulse","roost");
+    "ice punch","fire punch","psychic","rock slide","shadow ball","dragon pulse","roost","agility","dragon dance","v-create");
 
     public Move(String name) {
         checkValidMove(name);
         switch (name.toLowerCase()) {
             case "slash":
-                setMoveAttributes(name, "normal",70, 1,0, false);
+                setMoveAttributes(name, "normal",70, 1,0, 0,0);
                 break;
             case "flamethrower":
-                setMoveAttributes(name, "fire",95, 1, 0, false);
+                setMoveAttributes(name, "fire",95, 1, 0, 0,0);
                 break;
             case "earthquake":
-                setMoveAttributes(name, "ground",100, 1, 0, false);
+                setMoveAttributes(name, "ground",100, 1, 0, 0,0);
                 break;
             case "solar beam":
-                setMoveAttributes(name, "grass",120, 0.8, 0, false);
+                setMoveAttributes(name, "grass",120, 0.8, 0, 0,0);
                 break;
             case "hydro pump":
-                setMoveAttributes(name, "water",120, 0.8, 0, false);
+                setMoveAttributes(name, "water",120, 0.8, 0, 0,0);
                 break;
             case "skull bash":
-                setMoveAttributes(name, "normal",100, 1, 0, false);
+                setMoveAttributes(name, "normal",100, 1, 0, 0,0);
                 break;
             case "ice beam":
-                setMoveAttributes(name, "ice",90, 1, 0, false);
+                setMoveAttributes(name, "ice",90, 1, 0, 0,0);
                 break;
             case "swords dance":
-                setMoveAttributes(name, "normal",0, 1, 0, true);
+                setMoveAttributes(name, "normal",0, 1, 0, 1,0);
                 break;
             case "recover":
-                setMoveAttributes(name, "normal",0, 1, 0.5, false);
+                setMoveAttributes(name, "normal",0, 1, 0.5, 0,0);
                 break;
             case "thunderbolt":
-                setMoveAttributes(name, "electric", 95, 1, 0, false);
+                setMoveAttributes(name, "electric", 95, 1, 0, 0,0);
                 break;
             case "surf":
-                setMoveAttributes(name, "water", 95, 1, 0, false);
+                setMoveAttributes(name, "water", 95, 1, 0, 0,0);
                 break;
             case "sludge bomb":
-                setMoveAttributes(name, "poison", 90, 1, 0, false);
+                setMoveAttributes(name, "poison", 90, 1, 0, 0,0);
                 break;
             case "air slash":
-                setMoveAttributes(name, "flying", 75, 0.95, 0, false);
+                setMoveAttributes(name, "flying", 75, 0.95, 0, 0,0);
                 break;
             case "cross chop":
-                setMoveAttributes(name, "fighting", 100, 0.8, 0, false);
+                setMoveAttributes(name, "fighting", 100, 0.8, 0, 0,0);
                 break;
             case "thunder punch":
-                setMoveAttributes(name, "electric", 75, 1, 0, false);
+                setMoveAttributes(name, "electric", 75, 1, 0, 0,0);
                 break;
             case "fire punch":
-                setMoveAttributes(name, "fire", 75, 1, 0, false);
+                setMoveAttributes(name, "fire", 75, 1, 0, 0,0);
                 break;
             case "ice punch":
-                setMoveAttributes(name, "ice", 75, 1, 0, false);
+                setMoveAttributes(name, "ice", 75, 1, 0, 0,0);
                 break;
             case "psychic":
-                setMoveAttributes(name, "psychic", 90, 1, 0, false);
+                setMoveAttributes(name, "psychic", 90, 1, 0, 0,0);
                 break;
             case "rock slide":
-                setMoveAttributes(name, "rock", 75, 0.9, 0, false);
+                setMoveAttributes(name, "rock", 75, 0.9, 0, 0,0);
                 break;
             case "shadow ball":
-                setMoveAttributes(name, "ghost", 80, 1, 0, false);
+                setMoveAttributes(name, "ghost", 80, 1, 0, 0,0);
                 break;
             case "dragon pulse":
-                setMoveAttributes(name, "dragon", 85, 1, 0, false);
+                setMoveAttributes(name, "dragon", 85, 1, 0, 0,0);
                 break;
             case "roost":
-                setMoveAttributes(name, "flying", 0, 1, 0.5, false);
+                setMoveAttributes(name, "flying", 0, 1, 0.5, 0,0);
                 break;
+            case "agility":
+                setMoveAttributes(name, "psychic", 0, 1, 0, 0, 1);
+                break;
+            case "dragon dance":
+                setMoveAttributes(name, "dragon", 0, 1, 0, 0.5, 0.5);
+                break;
+            case "v-create":
+                setMoveAttributes(name, "fire", 180, 0.95, 0, -0.5, -0.5);
         }
     }
 
-    private void setMoveAttributes(String name, String type, int damage, double accuracy, double heal, boolean attackBoost) {
+    private void setMoveAttributes(String name, String type, int damage, double accuracy, double heal, double attackBoost, double speedBoost) {
         this.name = name;
         this.type = new Type(type);
         this.damage = damage;
         this.accuracy = accuracy;
         this.heal = heal;
         this.attackBoost = attackBoost;
+        this.speedBoost = speedBoost;
     }
 
     private void checkValidMove(String name) {
@@ -116,12 +126,16 @@ public class Move {
         return heal;
     }
 
-    public boolean getAttackBoost() {
+    public double getAttackBoost() {
         return attackBoost;
     }
 
     public double getAccuracy() {
         return accuracy;
+    }
+
+    public double getSpeedBoost() {
+        return speedBoost;
     }
 
     public String toString() {
